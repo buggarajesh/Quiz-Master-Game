b2c900b1c69fbbaa71e76355cd416287c664f6f3
Rajesh Bugga <110104629+buggarajesh@users.noreply.github.com>
2025-02-25 16:55:02 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,6 @@ import { Provider } from "react-redux"
 import { store, persistor } from "./redux/store"
 import { PersistGate } from "redux-persist/integration/react";
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
