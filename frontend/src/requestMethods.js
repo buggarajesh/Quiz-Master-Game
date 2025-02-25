b9277a9fa17446b2dc1a7b71ff192942086905e4
Rajesh Bugga <110104629+buggarajesh@users.noreply.github.com>
2025-02-25 13:22:53 +0530

    Update requestMethods.js

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://quiz-master-game.vercel.app/";
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
